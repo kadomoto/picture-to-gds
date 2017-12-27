92b830a883f6754472139fd3635cedf327b4e8ef
ourfool <jun-kadomoto@jcom.home.ne.jp>
2017-12-27 16:44:49 +0900

    Add support for specifying the size of the unit-cell and the layer number

--- picToGDS.py
+++ picToGDS.py
@@ -8,7 +8,7 @@ import numpy as np
 from gdsCAD import *
 
 
-def unit_cell(x, y):
+def unit_cell(x, y, layerNum):
     """Make a unit-cell
 
     Args:
@@ -18,14 +18,14 @@ def unit_cell(x, y):
         cell: Description
     """
     cell = core.Cell('VIA')
-    square = shapes.Rectangle((0.0, 0.0), (1.0, 1.0), layer=3)
+    square = shapes.Rectangle((0.0, 0.0), (1.0, 1.0), layer=(int)(layerNum))
     d = (x, y)
     cell.add(utils.translate(square, d))
 
     return cell
 
 
-def main(fileName):
+def main(fileName, sizeOfTheCell, layerNum):
     """Convert an image file (fileName) to a GDS file
     """
     print("Convert an image file to a GDS file.")
@@ -42,28 +42,47 @@ def main(fileName):
 
     ret, th2 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
 
+    # Fill orthological corner
+    for x in range(width - 1):
+        for y in range(height - 1):
+            if th2.item(y, x) == 0 and th2.item(y + 1, x) == 255 \
+                    and th2.item(y, x + 1) == 255 and th2.item(y + 1, x + 1) == 0:
+                th2.itemset((y + 1, x), 0)
+            elif th2.item(y, x) == 255 and th2.item(y + 1, x) == 0 \
+                    and th2.item(y, x + 1) == 0 and th2.item(y + 1, x + 1) == 255:
+                th2.itemset((y + 1, x + 1), 0)
+
     # Output image.bmp
     cv2.imwrite("image.bmp", th2)
 
     grid = core.Cell("GRID")
 
-    for i in range(width):
-        for j in range(height):
-            if th2.item(j, i) == 0:
-                print("({0}, {1}) is black".format(i, j))
-                grid.add(unit_cell(i, height - j - 1))
+    for x in range(width):
+        for y in range(height):
+            if th2.item(y, x) == 0:
+                print("({0}, {1}) is black".format(x, y))
+                grid.add(unit_cell(x, height - y - 1, layerNum))
+
+    scaledGrid = core.CellReference(
+        grid, origin=(0, 0), magnification=(float)(sizeOfTheCell))
+
+    top = core.Cell("TOP")
+    top.add(scaledGrid)
 
     # Add the copied cell to a layout and save
     layout = core.Layout("LIBRARY")
-    layout.add(grid)
+    layout.add(top)
     layout.save("image.gds")
 
 
 if __name__ == "__main__":
     args = sys.argv
-    if len(args) == 2:
+    if len(args) == 4:
         fileName = args[1]
-        main(fileName)
+        sizeOfTheCell = args[2]
+        layerNum = args[3]
+        main(fileName, sizeOfTheCell, layerNum)
     else:
-        print("usage: picToGDS.py <fileName>")
+        print(
+            "usage: python picToGDS.py <fileName> <sizeOfTheCell[um]> <layerNum>")
         quit()
